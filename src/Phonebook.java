450915019297973a8a95078d5e6891aefbad5639
CincoSwim <cgpucko@gmail.com>
2018-04-04 22:37:53 -0400

    Added a bit of logic towards command entry.
    This is gonna get spaghettied to shit.

--- src/Phonebook.java
+++ src/Phonebook.java
@@ -3,13 +3,43 @@ import java.io.*;
 
 public class Phonebook {
     public static void main(String[] foobar){
-        int entryIndex;
+        int index;
         Entry[] entryList;
-        String nameEntry, notesEntry, numberTempInput;
+        String nameEntry, notesEntry,commandEntry, numberTemp, searchKey;
+        char quitCondition;
         long numberEntry;
 
+        entryList = new Entry[200];
+        index = 0;
+        quitCondition = 'a';
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter: e | List: l | Find: f | Quit: q");
 
+        while (!(quitCondition == 'q')){
+            System.out.print("Command: ");
+            commandEntry = input.nextLine();
+
+            switch (commandEntry.charAt(0)){
+                case 'e':
+                    nameEntry = commandEntry.substring(2);
+                    System.out.print("Enter Number: ");
+                    numberTemp = input.nextLine();
+                    numberEntry = Long.parseLong(numberTemp.replaceAll("[^0-9]", ""));
+                    System.out.println("Enter Notes: ");
+                    notesEntry = input.nextLine();
+
+                    entryList[index] = new Entry(nameEntry,numberEntry,notesEntry);
+                case 'f':
+                    searchKey = commandEntry.substring(2);
+                case 'l':
+                case 'q':
+                    quitCondition = 'q';
+                    System.out.println("Saving and Shutting Down......");
+
+            }
+
+        }
 
     }
 
