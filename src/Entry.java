5fb2d5beb479b21a9a508e6e7f08212fcd5850f8
CincoSwim <cgpucko@gmail.com>
2018-04-05 23:39:31 -0400

    Everything in this should work except for reading the previous entries.
    Should.

--- src/Entry.java
+++ src/Entry.java
@@ -19,14 +19,19 @@ public class Entry {
         }
     }
     public static boolean findEntry(String query, Entry[] entryList, int index){
-
-        for (int i = 0; i < 200; i++){
-            if (entryList[i].name.equals(query)){
+        boolean entryFound;
+        entryFound = false;
+        for (int i = 0; i < index; i++){
+            if (entryList[i].name.contains(query)){
+                System.out.println(entryList[i].name);
+                System.out.println(String.valueOf(entryList[i].number).replaceFirst
+                        ("(\\d{3})(\\d{3})(\\d+)", "($1)-$2-$3"));
+                System.out.println();
+                entryFound = true;
             }
         }
 
-        //make logic for this
-        return true;
+        return entryFound;
     }
 
 }
--- src/Phonebook.java
+++ src/Phonebook.java
@@ -2,40 +2,59 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Phonebook {
-    public static void main(String[] foobar){
-        int index;
-        Entry[] entryList;
-        String nameEntry, notesEntry,commandEntry, numberTemp, searchKey;
+    static int index;
+    static Entry[] entryList;
+
+    public static void main(String[] foobar) throws FileNotFoundException {
+
+        String nameEntry, notesEntry, commandEntry, numberTemp, query;
         char quitCondition;
         long numberEntry;
+        boolean qSuccess;
 
         entryList = new Entry[200];
         index = 0;
         quitCondition = 'a';
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Loading...");
+        ReadPhoneBook();
+        System.out.println("Now Loaded!");
+        System.out.println();
         System.out.println("Enter: e | List: l | Find: f | Quit: q");
 
-        while (!(quitCondition == 'q')){
+        while (!(quitCondition == 'q')) {
             System.out.print("Command: ");
             commandEntry = input.nextLine();
 
-            switch (commandEntry.charAt(0)){
+            switch (commandEntry.charAt(0)) {
                 case 'e':
                     nameEntry = commandEntry.substring(2);
                     System.out.print("Enter Number: ");
                     numberTemp = input.nextLine();
                     numberEntry = Long.parseLong(numberTemp.replaceAll("[^0-9]", ""));
-                    System.out.println("Enter Notes: ");
+                    System.out.print("Enter Notes: ");
                     notesEntry = input.nextLine();
 
-                    entryList[index] = new Entry(nameEntry,numberEntry,notesEntry);
+                    entryList[index] = new Entry(nameEntry, numberEntry, notesEntry);
+                    index++;
+                    break;
+
                 case 'f':
-                    searchKey = commandEntry.substring(2);
+                    query = commandEntry.substring(2);
+                    qSuccess = Entry.findEntry(query, entryList, index);
+                    if (!qSuccess)
+                        System.out.println("No matching entries.");
+                    break;
                 case 'l':
+                    Entry.listEntries(entryList, index);
+                    break;
+
                 case 'q':
                     quitCondition = 'q';
                     System.out.println("Saving and Shutting Down......");
+                    WritePhoneBook();
+                    System.exit(0);
 
             }
 
@@ -43,4 +62,18 @@ public class Phonebook {
 
     }
 
+    public static void WritePhoneBook() throws FileNotFoundException {
+        PrintStream P = new PrintStream("phonebook.txt");
+        for (int i = 0; i < index; i++) {
+            P.println(entryList[i].name + "\t" +
+                    entryList[i].number + "\t" +
+                    entryList[i].notes);
+        }
+        P.close();
+        System.out.println("Phonebook Saved");
+    }
+
+    public static void ReadPhoneBook() throws FileNotFoundException{
+       // Scanner filein = new Scanner ();
+    }
 }
