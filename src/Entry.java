d3b9111ef858650de7c12fb72ba4f64530d87f86
CincoSwim <cgpucko@gmail.com>
2018-04-05 21:09:28 -0400

    Added a bit of logic towards command entry.
    This is gonna get spaghettied to shit.

--- src/Entry.java
+++ src/Entry.java
@@ -10,4 +10,23 @@ public class Entry {
         number = numberEntry;
         notes = notesEntry;
     }
+    public static void listEntries(Entry[] entryList, int index){
+        for (int i = 0; i < index; i++){
+            System.out.println("Name: " + entryList[i].name);
+            System.out.println("Phone Number: " +entryList[i].number);
+            System.out.println("Notes: " + entryList[i].notes);
+            System.out.println();
+        }
+    }
+    public static boolean findEntry(String query, Entry[] entryList, int index){
+
+        for (int i = 0; i < 200; i++){
+            if (entryList[i].name.equals(query)){
+            }
+        }
+
+        //make logic for this
+        return true;
+    }
+
 }
